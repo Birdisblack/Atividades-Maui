af3dad254985ef16cec8506b2deb35053bdd5e90
Black Bird <wesley100skate@hotmail.com>
2024-10-23 09:46:37 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombustivel/MauiAppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic;
+
+namespace MauiAppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+     
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try 
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                if(etanol <= (gasolina * 0.7)) 
+                {
+                    MsgBoxResult = "O etanol está compensando";
+                }
+                else                
+                {
+                    MsgBoxResult = "A Gasolina está compensando";
+                }
+                DisplayAlert("Calculado", msg, "OK");
+
+            }            
+            catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Fecha");
+            }
+
+
+
+        }
+
+
+
+    }
+
+}
